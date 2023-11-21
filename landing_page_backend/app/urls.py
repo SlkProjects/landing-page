7b39b4c9082c33679a625caec5fbfa2faecaef18
Sam Luc <lukekatshinda@gmail.comgit config user.email lukekatshinda@gmail.com>
2023-11-21 15:17:18 +0200

    Logos fetch on frontend

--- landing_page_backend/app/urls.py
+++ landing_page_backend/app/urls.py
@@ -1,13 +1,8 @@
 # app/urls.py
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 from .views import TrustedPartnerListView
 
+# app/function urls
 urlpatterns = [
     path('logos/', TrustedPartnerListView.as_view(), name='partners-list'),
 ]
-
-# work around to gain access to images during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- landing_page_backend/landing_page_backend/urls.py
+++ landing_page_backend/landing_page_backend/urls.py
@@ -1,10 +1,17 @@
 """
 URL configuration for landing_page_backend project.
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+# Backend access points
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
 ]
+
+# work around to gain access to images during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
